6e93bdfa8d1459b485e96e2a2b20fec2472cdc1b
Alex <alex.web.architect@gmail.com>
2017-02-05 14:13:08 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function randomQuote() {
   $.ajax({
-      url: "https://crossorigin.me/http://api.forismatic.com/api/1.0/?",
+      url: "http://api.forismatic.com/api/1.0/?",
       dataType: "jsonp",
       data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
       success: function( response ) {
